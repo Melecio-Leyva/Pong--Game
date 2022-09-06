40014440979ed91757cff46275c097c05af5934b
Mele <61437633+Melecio-Leyva@users.noreply.github.com>
2022-09-06 13:24:24 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,18 +8,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
-
+    #update the current scores and align them on opposite sides of eachother
     def update(self):
         self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
         self.goto(100, 200)
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
-
+    # score point for left paddle function
     def l_point(self):
         self.l_score += 1
         self.update()
-
+    # score point for right paddle function
     def r_point(self):
         self.r_score += 1
         self.update()
