3b20a18a0132e6744161625d07a5b4a585d66f2b
Mele <61437633+Melecio-Leyva@users.noreply.github.com>
2022-09-06 13:07:35 -0700

    Pong Game project
    Day 22 of 100 days of programming python.

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+ball = Ball()
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+scoreboard = Scoreboard()
+screen.listen()
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        # bounce the ball
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # right middle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    # if left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+screen.exitonclick()
